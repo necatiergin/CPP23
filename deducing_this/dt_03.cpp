6b5c50572f3957e976f47ded313cbf8492cd5e75
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-10-01 22:50:28 +0300

    Create dt_03.cpp

---
+++ deducing_this/dt_03.cpp
@@ -0,0 +1,44 @@
+#include <optional>
+
+template <typename T>
+class Optional {
+public:
+    bool has_value()const;
+
+    // version of value for non-const lvalues
+    constexpr T& value()&
+    {
+        if (has_value()) {
+            return this->m_value;
+        }
+        throw std::bad_optional_access();
+    }
+
+    // version of value for const lvalues
+    constexpr T const& value() const&
+    {
+        if (has_value()) {
+            return this->m_value;
+        }
+        throw std::bad_optional_access();
+    }
+
+    // version of value for non-const rvalues... are you bored yet?
+    constexpr T&& value()&&
+    {
+        if (has_value()) {
+            return std::move(this->m_value);
+        }
+        throw std::bad_optional_access();
+    }
+
+    // you sure are by this point
+    constexpr T const&& value() const&&
+    {
+        if (has_value()) {
+            return std::move(this->m_value);
+        }
+        throw std::bad_optional_access();
+    }
+    // ...
+};
